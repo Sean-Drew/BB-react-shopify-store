6914dc59dec5ba6be4aec479e19c3ea3c0ea54cb
Sean-Drew <SDrew989@gmail.com>
2021-01-24 15:49:41 -0500

    NavBar items
    Added logo svg to public file. Imported logo svg to NavBar component.
    Added menu & shopping cart icons to NavBar. Styling for all of these.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react'
 import { Flex, Icon, Image, Text } from '@chakra-ui/react'
 import { ShopContext } from '../context/shopProvider'
+import { MdMenu, MdShoppingBasket } from 'react-icons/md'
 
 const NavBar = () => {
     
     const { openCart, openMenu, checkout } = useContext(ShopContext)
     
     return (
-        <Flex flexDir='row' justifyContent='space-between' p='2rem'>
-            <Text>Menu</Text>
-            <Text>Logo</Text>
-            <Text>Cart</Text>
+        <Flex backgroundColor='#FFA8E2' flexDir='row' justifyContent='space-between' p='2rem'>
+            <Icon fill='white' cursor='pointer' as={MdMenu} w={30} h={30} />
+            <Image img src={'/Logologo_1.svg'} w={100} h={100}/>
+            <Icon fill='white' cursor='pointer' as={MdShoppingBasket} w={30} h={30} />
         </Flex>
     )
 }
